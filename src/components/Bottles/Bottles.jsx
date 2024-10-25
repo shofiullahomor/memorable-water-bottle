edd882464d07fe0ab81911931ebde3cdb03dc95d
shofiullah omor <shofiullahomor1436@gmail.com>
2024-10-25 07:36:44 +0600

    memorable water bottle

---
+++ src/components/Bottles/Bottles.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import Bottle from "../Bottle/Bottle";
+import "./Bottles.css";
+
+const Bottles = () => {
+  const [bottles, setBottles] = useState([]);
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    fetch("bottles.json")
+      .then((res) => res.json())
+      .then((data) => setBottles(data));
+  }, []);
+  const handleAddToCart = (bottle) => {
+    const newCart = [...cart, bottle];
+    setCart(newCart);
+  };
+
+  return (
+    <>
+      <div>
+        <h2>Bottles Available: {bottles.length}</h2>
+        <h4>Cart: {cart.length}</h4>
+        <div className="bottle-container">
+          {bottles.map((bottle) => (
+            <Bottle
+              key={bottle.id}
+              handleAddToCart={handleAddToCart}
+              bottle={bottle}
+            ></Bottle>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Bottles;
